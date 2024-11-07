da8d1b067841d48308af73740331a6343889240f
amandinha06 <a.dinjz06@gmail.com>
2024-11-06 21:29:59 -0300

    atualização

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ const input = document.querySelector("#input");
 const counter = document.querySelector(".counter");
 
 input.addEventListener("input", () => {
-    let count = 0;
-
-    if(count === 0) {
-        count= input.value.length;
-
-        counter.textContent = `${count} Caracteres`;
-    }
+  counter.textContent = `${input.value.length} Caracteres`;
 });
 
+function resetar() {
+  input.value = '';
+  counter.textContent = '0 Caracteres';
+}
